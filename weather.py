7293f0b6066e95288059aecbe4d12414377956bb
Wei Yang <wei@weizilla.com>
2016-02-12 03:03:57 +0000

    add to lcd

--- weather.py
+++ weather.py
@@ -1,18 +1,25 @@
 import time
 from Adafruit_BME280 import *
+import Adafruit_CharLCD as LCD
 
 sensor = BME280(mode=BME280_OSAMPLE_8)
+lcd = LCD.Adafruit_CharLCDPlate()
 
 while(True): 
-    degrees = sensor.read_temperature()
+    degrees = sensor.read_temperature() * 1.8 + 32
     pascals = sensor.read_pressure()
     hectopascals = pascals / 100
     humidity = sensor.read_humidity()
 
-    print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
-    print 'Temp      = {0:0.3f} deg C'.format(degrees)
-    print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-    print 'Humidity  = {0:0.2f} %'.format(humidity)
+    lcd.clear()
+    lcd.set_color(1.0, 0.0, 0.0)
+    lcd.message("Temp = {0:0.3f}\nHumidity = {1:0.2f} %"
+        .format(degrees, humidity))
+
+    #print 'Timestamp = {0:0.3f}'.format(sensor.t_fine)
+    #print 'Temp      = {0:0.3f} deg C'.format(degrees)
+    #print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+    #print 'Humidity  = {0:0.2f} %'.format(humidity)
 
     try:
         time.sleep(1)
