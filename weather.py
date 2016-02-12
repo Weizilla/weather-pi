3af1bc6b3a95ef2756212dea9ace6efc4a9f328f
Wei Yang <wei@weizilla.com>
2016-02-12 03:25:06 +0000

    remove lcd stuff

--- weather.py
+++ weather.py
@@ -5,14 +5,18 @@ import Adafruit_CharLCD as LCD
 sensor = BME280(mode=BME280_OSAMPLE_8)
 lcd = LCD.Adafruit_CharLCDPlate()
 
+button = False
+backlight = True
+
+lcd.clear()
+lcd.set_color(1.0, 0.0, 0.0)
+
 while(True): 
     degrees = sensor.read_temperature() * 1.8 + 32
     pascals = sensor.read_pressure()
     hectopascals = pascals / 100
     humidity = sensor.read_humidity()
 
-    lcd.clear()
-    lcd.set_color(1.0, 0.0, 0.0)
     lcd.message("Temp = {0:0.3f}\nHumidity = {1:0.2f} %"
         .format(degrees, humidity))
 
@@ -24,4 +28,5 @@ while(True):
     try:
         time.sleep(1)
     except:
+        lcd.clear()
         break
